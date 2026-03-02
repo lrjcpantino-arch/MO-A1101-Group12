c0c80ee0e4e124d28d03fa35d50aae795d88b954
lrjcpantino-arch <lr.jcpantino@mmdc.mcl.edu.ph>
2026-03-02 11:36:15 +0800

    Add files via upload

---
+++ MotorPh.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.motorph;
+
+/**
+ *
+ * @author Jiane Carlo Pantino
+ */
+public class MotorPh {
+    public static void main(String[] args) {
+       System.out.println("Hiiiii");
+       
+       
+       
+    }
+}
