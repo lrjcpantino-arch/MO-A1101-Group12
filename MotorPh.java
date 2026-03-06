97afef4308559332bf76fb4568f1b2e5d2444a91
lrjcpantino-arch <lr.jcpantino@mmdc.mcl.edu.ph>
2026-03-06 16:24:02 +0800

    Add files via upload

--- MotorPh.java
+++ MotorPh.java
@@ -3,6 +3,13 @@
  */
 
 package com.mycompany.motorph;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 /**
  *
@@ -10,9 +17,71 @@ package com.mycompany.motorph;
  */
 public class MotorPh {
     public static void main(String[] args) {
-       System.out.println("Hiiiii");
+        
+        Scanner mphd= new Scanner(System.in);
+        
+        String empDetails = "resources/MPHD Employee Details.csv";
+        
+        System.out.println("------------WELCOME TO MOTORPH PAYROLL SYSTEM------------");
+        
+        
+        System.out.println("Enter Employee ID");
+        String inputId= mphd.nextLine();
+        
+        System.out.println("Enter Password");
+        String inputPassword= mphd.nextLine();
+        
+        boolean loginSuccess = false;
+        boolean isAdmin = false;
+        String employeeName = "";
+        
+        String adminId= "13579";
+        String adminPassword= "12345";
+        
+        if (inputId.equals(adminId)&& inputPassword.equals(adminPassword)) {
+        loginSuccess= true;
+                isAdmin= true;
+        }
+        if(!loginSuccess) {
+        try (BufferedReader br = new BufferedReader(new FileReader(empDetails))){
+        br.readLine();
+        
+        String line;
+        while ((line = br.readLine())!= null){
+        if(line.trim().isEmpty()) continue;
+        String[] data = line.split(",");
+        String empNo = data[0];
+        String lastName = data[1];
+        String password = data[2];
+        
+        if (inputId.equals(empNo)&& inputPassword.equals(password)){
+        loginSuccess = true;
+        employeeName = lastName;
+        break;
+        }
+        }
+        
+        } catch(Exception e){
+        System.out.println("Error reading employee file.");
+        return;
+        }
+        }
+        if (loginSuccess){
+        if (isAdmin){
+        System.out.println("Login Successful");
+        System.out.println("Welcome Administrator");
+        }
+        else{
+        System.out.println("Login Successful");
+        System.out.println("Welcome"+ employeeName);
+        System.out.println("Employee Access Granted");
+        }
+        }
+        else{
+        System.out.println("Login Failed");
+        }
        
-       
-       
+        
+        
     }
 }
